ef98fe36652a8ed79ebb088bd5e01b01f19852e7
fbriou <fbriou@gmail.com>
2019-03-05 18:44:49 +0100

    update default

--- src/default_data/index.js
+++ src/default_data/index.js
@@ -2,19 +2,10 @@ module.exports = [
     {
         name: 'navbar_simple',
         storeName: {
-            en: "Simple navbar"
+            en: "JLL navbar"
         },
         storeDesc: {
-            en: "Simple navbar"
+            en: "JLL navbar"
         }
-    },
-    // {
-    //     name: 'navbar_center',
-    //     storeName: {
-    //         en: "Centered navbar"
-    //     },
-    //     storeDesc: {
-    //         en: "Centered navbar"
-    //     }
-    // }
+    }
 ]
